f04602c8007f07315048c4dbcc2289a5ed22f21e
Noah Kaplan <noahkaplan4@gmail.com>
2013-12-20 18:47:41 -0800

    Last changes of git test

--- calc.rb
+++ calc.rb
@@ -1,15 +1,15 @@
 puts "Enter a number, a + or - sign, then another number please."
 
 firstNum = gets.to_i
-
+# Useless comment
 userOperator = gets.chomp
 
 secondNum = gets.to_i
 
 if userOperator == "+"
-	puts "Result: #{firstNum + secondNum}"
+  puts "Result: #{firstNum + secondNum}"
 elsif userOperator == "-"
-	puts "Result: #{firstNum - secondNum}"
+  puts "Result: #{firstNum - secondNum}"
 else
-	puts "error: invalid operator!"
+  puts "error: invalid operator!"
 end
